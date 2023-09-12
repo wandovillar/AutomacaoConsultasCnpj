5a4302d0d62e4b4f164f7505ed4c1168ba2f09d4
357382sp <wandovillar@gmail.com>
2023-09-12 19:11:06 -0300

    Seu mensagem de commit

---
+++ .history/ConsultasCnpj_20230912154503.py
@@ -0,0 +1,46 @@
+import requests
+import pandas as pd
+import json
+import time
+  
+
+
+
+
+
+
+# Função para consultar um CNPJ e retornar os dados em JSON
+def consultar_cnpj(cnpj):
+    url = f"https://publica.cnpj.ws/cnpj/{cnpj}"
+    response = requests.get(url)
+    if response.status_code == 200:
+        return response.json()
+    elif response.status_code == 429:
+        # Lidar com a limitação de taxa, aguardar o tempo especificado na mensagem de erro
+        wait_time = int(response.headers.get('Retry-After', 60))
+        print(f'Limite de taxa atingido. Aguardando {wait_time} segundos...')
+        time.sleep(wait_time)
+        return consultar_cnpj(cnpj)  # Tentar novamente após a espera
+    else:
+        print(f'Erro ao consultar CNPJ {cnpj}: {response.status_code}')
+        return None
+
+# Ler a planilha XLSX com a lista de CNPJs
+df = pd.read_excel('ListaCnpj.xlsx')
+
+# Inicializar uma lista para armazenar os dados de todos os CNPJs
+dados_cnpjs = []
+
+# Loop através dos CNPJs e fazer as consultas
+for cnpj in df['CNPJ']:
+    dados = consultar_cnpj(cnpj)
+    if dados:
+        dados_cnpjs.append(dados)
+
+# Salvar todos os dados em um único arquivo JSON
+with open('dados_cnpjs.json', 'w', encoding='utf-8') as json_file:
+    json.dump(dados_cnpjs, json_file, ensure_ascii=False, indent=4)
+
+print(f'Dados de {len(dados_cnpjs)} CNPJs foram salvos em "dados_cnpjs.json".')
+
+
---
+++ .history/setup_exe_20230912155000.py
@@ -0,0 +1,28 @@
+import sys
+from cx_Freeze import setup, Executable
+
+# Inclua todas as dependências necessárias (módulos importados) aqui
+includes = []
+
+# Crie o executável
+executables = [
+    Executable('ConsultasCnpj.py', base=None)
+]
+
+# Configurações para a construção do executável
+build_exe_options = {
+    "includes": includes,
+    "excludes": [],
+    "packages": [],
+    "include_files": [],
+}
+
+setup(
+    name="ConsultasCnpj",
+    version="1.0",
+    description="",
+    options={
+        "build_exe": build_exe_options,
+    },
+    executables=executables
+)
